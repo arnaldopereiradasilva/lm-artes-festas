0307c1bcd9f26de26eae803fc1115ef50c619c47
Arnaldo Pereira da Silva <labassuriademanto@gmail.com>
2026-06-12 13:46:19 -0300

    Add trust proxy for Render (secure cookie behind proxy)

--- server/app.js
+++ server/app.js
@@ -19,6 +19,8 @@ if (!process.env.SESSION_SECRET) {
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
